29bb69a522001ac0a2af96ca7b0f07379e645f06
Chris Bow <chris.bow@shaw.ca>
2021-02-13 16:42:18 -0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+
+#Create a route (Decorator)
+
+@app.route('/')
+def index():
+	first_name = "Chris"
+	stuff = "This is <strong>Bold</strong> Text"
+
+	favorite_pizza = ["Peperoni", "Cheese", "Mushrooms", 41]
+	return render_template('index.html', 
+		first_name=first_name,
+		stuff = stuff,
+		favorite_pizza = favorite_pizza,
+		)
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', user_name=name)
+
+# Create Custom error Pages
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+#Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
